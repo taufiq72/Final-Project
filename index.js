96813c36c362039614a4b546db716f5536408770
taufiq72 <taufiqq1207@gmail.com>
2026-08-24 23:44:43 +0700

    debug

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const cors = require('cors');
 require('dotenv').config();
 
 const routes = require('./routes');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Route root untuk tes kesehatan API di browser
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to IPGuard Risk Intelligence API' });
+  res.json({
+    message: 'IPGuard SaaS API status ONLINE',
+    version: '1.0.0'
+  });
 });
 
+// Mounting router utama
 app.use('/api', routes);
 
 const PORT = process.env.PORT || 3000;
